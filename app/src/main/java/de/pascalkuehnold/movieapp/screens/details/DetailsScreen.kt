0c29695698f9d0dfea2e864b582401f4bc7aaf06
Pascal <Pascal.Kuehnold@gmail.com>
2022-07-15 03:27:54 +0200

    changed the detailed screen

--- app/src/main/java/de/pascalkuehnold/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/de/pascalkuehnold/movieapp/screens/details/DetailsScreen.kt
@@ -1,11 +1,6 @@
 package de.pascalkuehnold.movieapp.screens.details
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -15,13 +10,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import de.pascalkuehnold.movieapp.model.Movie
+import de.pascalkuehnold.movieapp.model.getMovies
+import de.pascalkuehnold.movieapp.widgets.HorizontalImageCarousel
+import de.pascalkuehnold.movieapp.widgets.MovieRow
 
 
 /**
  * Created by Pascal Kühnold on 7/15/2022.
  */
 @Composable
-fun DetailsScreen(navController: NavController, movieTitle: String?) {
+fun DetailsScreen(navController: NavController, movieId: String?) {
+
+    val movie: Movie = getMovies().first {
+        it.id == movieId
+    }
 
     Scaffold(
         topBar = {
@@ -50,14 +53,16 @@ fun DetailsScreen(navController: NavController, movieTitle: String?) {
                 .fillMaxWidth()
         ) {
             Column(
-                verticalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(
-                    text = movieTitle.toString(),
-                    style = MaterialTheme.typography.h5
-                )
+                MovieRow(movie = movie)
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images", style = MaterialTheme.typography.h5)
+                HorizontalImageCarousel(movie)
             }
         }
     }
 }
+
